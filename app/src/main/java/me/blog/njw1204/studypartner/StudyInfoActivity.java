9360ce9728d621859ea426a6735b48b92a905776
YoungSeok Joo <robinjoo1015@naver.com>
2018-10-31 18:45:27 +0900

    Study info activity make

---
+++ app/src/main/java/me/blog/njw1204/studypartner/StudyInfoActivity.java
@@ -0,0 +1,13 @@
+package me.blog.njw1204.studypartner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudyInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_info);
+    }
+}
